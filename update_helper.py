4dca32c14ebb20507f904452d6a6d73a902bf46c
Torque (Tareq) El Dandachi <tareqdandachi@gmail.com>
2022-07-26 23:22:09 -0400

    catch no model file?

--- update_helper.py
+++ update_helper.py
@@ -19,82 +19,88 @@ if __name__=="__main__":
         files_asy = []
         files_lib = []
         
-        models = open("models.md", "r").read()
+        if os.path.exists("models.md"):
         
-        last_dir = []
-        dest_dir = []
-        last_dir_size = [-1]
+            models = open("models.md", "r").read()
+            
+            last_dir = []
+            dest_dir = []
+            last_dir_size = [-1]
 
-        for line in models.split("\n"):
-            
-            indents = (len(line) - len(line.lstrip()))
-            
-            loc = line.split("- ")[1].split(":")[0]
-            
-            if ":" in line:
-                name = line.split("- ")[1].split(":")[1].lstrip().rstrip()
-                path = line.split("- ")[1].split(":")[0].lstrip().rstrip()
-            elif is_file(line):
+            for line in models.split("\n"):
+                
+                indents = (len(line) - len(line.lstrip()))
+                
+                loc = line.split("- ")[1].split(":")[0]
                 
                 if ":" in line:
-                    name = line.split("- ")[1].split(":")[1].rstrip().lstrip()
-                    path = line.split("- ")[1].split(":")[0].rstrip().lstrip()
+                    name = line.split("- ")[1].split(":")[1].lstrip().rstrip()
+                    path = line.split("- ")[1].split(":")[0].lstrip().rstrip()
+                elif is_file(line):
+                    
+                    if ":" in line:
+                        name = line.split("- ")[1].split(":")[1].rstrip().lstrip()
+                        path = line.split("- ")[1].split(":")[0].rstrip().lstrip()
+                    else:
+                        name = line.split("- ")[1].rstrip().lstrip()
+                        path = name
                 else:
-                    name = line.split("- ")[1].rstrip().lstrip()
-                    path = name
-            else:
-                name = "."
-                path = line.split("- ")[1].lstrip().rstrip()
+                    name = "."
+                    path = line.split("- ")[1].lstrip().rstrip()
+                
+                if not is_file(name): 
+                    
+                    while indents < last_dir_size[-1]:
+                        
+                        last_dir = last_dir[:-1]
+                        dest_dir = dest_dir[:-1]
+                        last_dir_size = last_dir_size[:-1]
+                    
+                    if indents == last_dir_size[-1]:
+                        
+                        last_dir[-1] = path+"/"
+                        dest_dir[-1] = name+"/"
+                        
+                    else: # indents > prev_indents
+                        
+                        last_dir.append(path+"/")
+                        dest_dir.append(name+"/")
+                        last_dir_size.append(indents)
+                    
+                if ".asy" in name: files_asy.append((''.join(last_dir) + path, 
+                                                    (''.join(dest_dir)).replace("./", ""),
+                                                    name
+                                                    ))
+                if (".lib" in name or ".asc" in name): files_lib.append((''.join(last_dir) + path, 
+                                                                        (''.join(dest_dir)).replace("./", ""),
+                                                                        name
+                                                                        ))
             
-            if not is_file(name): 
+            for src, dest, dest_filename in files_asy:
                 
-                while indents < last_dir_size[-1]:
-                    
-                    last_dir = last_dir[:-1]
-                    dest_dir = dest_dir[:-1]
-                    last_dir_size = last_dir_size[:-1]
-                
-                if indents == last_dir_size[-1]:
-                    
-                    last_dir[-1] = path+"/"
-                    dest_dir[-1] = name+"/"
-                    
-                else: # indents > prev_indents
-                    
-                    last_dir.append(path+"/")
-                    dest_dir.append(name+"/")
-                    last_dir_size.append(indents)
-                
-            if ".asy" in name: files_asy.append((''.join(last_dir) + path, 
-                                                 (''.join(dest_dir)).replace("./", ""),
-                                                 name
-                                                 ))
-            if (".lib" in name or ".asc" in name): files_lib.append((''.join(last_dir) + path, 
-                                                                    (''.join(dest_dir)).replace("./", ""),
-                                                                    name
-                                                                    ))
-        
-        for src, dest, dest_filename in files_asy:
-            
-            os.makedirs(LTSPICE_LIB_PATH + "/sym/qnn-spice/" + dest, exist_ok=True)
+                os.makedirs(LTSPICE_LIB_PATH + "/sym/qnn-spice/" + dest, exist_ok=True)
 
-            os.symlink(os.getcwd() + "/" + src, LTSPICE_LIB_PATH + "/sym/qnn-spice/" + dest + dest_filename)
-            
-            print("    ADDED:", dest_filename)
-        
-        for src, dest, dest_filename in files_lib:
-            
-            os.makedirs(LTSPICE_LIB_PATH + "/sub/qnn-spice/" + dest, exist_ok=True)
-            
-            inner_symlink = LTSPICE_LIB_PATH + "/sub/qnn-spice/" + dest + dest_filename
-
-            os.symlink(os.getcwd() + "/" + src, inner_symlink)
-            
-            outer_symlink = LTSPICE_LIB_PATH + "/sub/" + dest_filename.split("/")[-1]
-            
-            if os.path.exists(outer_symlink):
-                os.unlink(outer_symlink)
+                os.symlink(os.getcwd() + "/" + src, LTSPICE_LIB_PATH + "/sym/qnn-spice/" + dest + dest_filename)
                 
-            os.symlink(inner_symlink, outer_symlink)
+                print("    ADDED:", dest_filename)
             
-            print("    ADDED:", dest_filename)
+            for src, dest, dest_filename in files_lib:
+                
+                os.makedirs(LTSPICE_LIB_PATH + "/sub/qnn-spice/" + dest, exist_ok=True)
+                
+                inner_symlink = LTSPICE_LIB_PATH + "/sub/qnn-spice/" + dest + dest_filename
+
+                os.symlink(os.getcwd() + "/" + src, inner_symlink)
+                
+                outer_symlink = LTSPICE_LIB_PATH + "/sub/" + dest_filename.split("/")[-1]
+                
+                if os.path.exists(outer_symlink):
+                    os.unlink(outer_symlink)
+                    
+                os.symlink(inner_symlink, outer_symlink)
+                
+                print("    ADDED:", dest_filename)
+                
+        else:
+            
+            print("SKIPPING: NO MODEL FILE IN DIRECTORY")
